29cf847effb73a0c981971cf59dd22814e6d4f88
logan <gnlee95@blocko.io>
2024-11-29 21:55:26 +0900

    about yield

---
+++ src/thread/control/printer/MyPrinter4.java
@@ -0,0 +1,60 @@
+package thread.control.printer;
+
+import java.util.Queue;
+import java.util.Scanner;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+import static util.MyLogger.log;
+
+/**
+ * Check 로직에 낭비되는 자원 낭비를 개선하기 위해 [ yield ] 추가
+ */
+public class MyPrinter4 {
+    public static void main(String[] args) throws InterruptedException{
+        Printer printer = new Printer();
+        Thread printerThread = new Thread(printer, "printer");
+        printerThread.start();
+
+        Scanner userInput = new Scanner(System.in);
+        while (true) {
+            System.out.println("프린터할 문서를 입력하세요. 종료 (q): ");
+            String input = userInput.nextLine();
+
+            if (input.equals("q")) {
+                printerThread.interrupt();
+                break;
+            }
+
+            printer.addJob(input);
+        }
+    }
+
+    static class Printer implements Runnable {
+        Queue<String> jobQueue = new ConcurrentLinkedQueue<>();
+
+        @Override
+        public void run() {
+            while(!Thread.interrupted()) {
+                if (jobQueue.isEmpty()) {
+                    Thread.yield();
+                    continue;
+                }
+
+                try {
+                    String job = jobQueue.poll();
+                    log("출력 시작: " +  job + ", 대기문서: " + jobQueue);
+                    Thread.sleep(3000);
+                    log("출력 완료: " + job);
+                } catch (InterruptedException e) {
+                    log("인터럽트 !");
+                    break;
+                }
+            }
+            log("프린터 종료");
+        }
+
+        public void addJob(String input) {
+            jobQueue.offer(input);
+        }
+    }
+}
